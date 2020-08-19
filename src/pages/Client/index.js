596f842ddfc734385429901413274aa07f3944cf
Felipe <felipe.olivera_@outlook.com>
2020-08-18 21:29:10 -0300

    .

--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Title, ContentForm, Input, ActionSave } from './styles';
 
+import api from '../../service/api';
+
 function Client() {
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [address_number, setAddress_number] = useState('');
+    const [phone, setPhone] = useState('');
+
+    async function save() {
+        await api.post(`/sistema/cadastrar-cliente`, {
+            name,
+            address,
+            phone,
+            address_number
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch(err => {
+                return err; 
+            })
+    }
+
     return (
         <Container>
             <Title>
@@ -10,23 +33,31 @@ function Client() {
             <ContentForm>
                 <Input>
                     <span>Nome</span>
-                    <input type="text"/>
+                    <input type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}/>
                 </Input>
                 <Input>
                     <span>Endereço</span>
-                    <input type="text"/>
-                </Input>
-                <Input>
-                    <span>Número</span>
-                    <input type="text"/>
+                    <input type="text"
+                    value={address}
+                    onChange={e => setAddress(e.target.value)}/>
                 </Input>
                 <Input>
                     <span>Telefone</span>
-                    <input type="text"/>
+                    <input type="text"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}/>
+                </Input>
+                <Input>
+                    <span>Número</span>
+                    <input type="text"
+                    value={address_number}
+                    onChange={e => setAddress_number(e.target.value)}/>
                 </Input>
                 <ActionSave>
                     <button type="button">Cancelar</button>
-                    <button type="button">Salvar</button>
+                    <button type="button" onClick={save}>Salvar</button>
                 </ActionSave>
             </ContentForm>
         </Container>
